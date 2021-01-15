34f532d5116485ac5a365f9aa4a48244f9aa946d
Sushmita Singh <40317759+SushmitaSingh96@users.noreply.github.com>
2021-01-15 14:17:30 +0530

    Update Brackets.py

--- Brackets.py
+++ Brackets.py
@@ -1,5 +1,3 @@
-My Solution:
-
 def solution(S):
     if S == "":
         return 1
@@ -31,21 +29,3 @@ def solution(S):
     if len(stack) == 0:
         return 1
     return 0
-    pass
-
-StackOverflow Solution:
-
-def solution(S):
-
-    matches, stack = dict(['()', '[]', '{}']), []
-
-    for i in S:
-        if i in matches.values():
-            if stack and matches[stack[-1]] == i:
-                stack.pop()
-            else:
-                return 0
-        else:
-            stack.append(i)
-
-    return int(not stack)
